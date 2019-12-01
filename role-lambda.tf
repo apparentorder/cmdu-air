1a658c932ae2cfc8b19ec10a7fc583770b7f2a13
apparentorder <github@neveragain.de>
2019-12-01 12:55:31 +0100

    split lambda-role and other IAM stuff into two files

---
+++ role-lambda.tf
@@ -0,0 +1,68 @@
+
+resource aws_iam_role air-lambda {
+	name = "air-lambda"
+	assume_role_policy = jsonencode({
+		"Version": "2012-10-17",
+		"Statement": [{
+			"Action": "sts:AssumeRole",
+			"Principal": {
+				"Service": "lambda.amazonaws.com"
+			},
+			"Effect": "Allow",
+			"Sid": ""
+		}]
+	})
+	tags = var.airtag
+}
+
+# ----------------------------------------------------------------------
+
+resource aws_iam_policy air-sns-publish {
+	name = "air-sns-publish"
+	policy = jsonencode({
+	    "Version": "2012-10-17",
+	    "Statement": [
+		{
+		    "Effect": "Allow",
+		    "Action": "sns:Publish",
+		    "Resource": "arn:aws:sns:eu-central-1:329261680777:air-notify"
+		}
+	    ]
+	})
+}
+
+resource aws_iam_policy air-sqs-receive {
+	name = "air-sqs-receive"
+	policy = jsonencode({
+	    "Version": "2012-10-17",
+	    "Statement": [
+		{
+		    "Effect": "Allow",
+		    "Action": [
+			"sqs:DeleteMessage",
+			"sqs:ReceiveMessage",
+			"sqs:GetQueueAttributes"
+		    ],
+		    "Resource": "arn:aws:sqs:eu-central-1:329261680777:airdata"
+		}
+	    ]
+	})
+}
+
+# ----------------------------------------------------------------------
+
+resource aws_iam_role_policy_attachment air-lambda-sns-publish {
+	role = "${aws_iam_role.air-lambda.name}"
+	policy_arn = "${aws_iam_policy.air-sns-publish.arn}"
+}
+
+resource aws_iam_role_policy_attachment air-lambda-sqs-receive {
+	role = "${aws_iam_role.air-lambda.name}"
+	policy_arn = "${aws_iam_policy.air-sqs-receive.arn}"
+}
+
+resource aws_iam_role_policy_attachment air-lambda-basic {
+	role = "${aws_iam_role.air-lambda.name}"
+	policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
